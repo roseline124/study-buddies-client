a536eec89298694738091a467facad1bce1fddea
roseline124 <guseod24@gmail.com>
2020-08-02 16:37:44 +0900

    Remove css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import './App.css'
-
 import React from 'react'
-import logo from './logo.svg'
+import logo from '../static/logo.svg'
 import Pages from './pages'
 
 const App = () => {
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <Pages />
     </div>
   )
