2c9072a0c4d33e8b531df06146f1b9aa94570428
roseline124 <guseod24@gmail.com>
2020-08-02 18:42:17 +0900

    Add login, home link to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Link } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import Logo from '../static/icons/Logo'
 
@@ -7,7 +7,12 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'space-between',
     height: 80,
+  },
+  logoWrapper: {
+    display: 'flex',
+    alignItems: 'center',
   },
   logo: {
     width: '3rem',
@@ -18,21 +23,35 @@ const useStyles = makeStyles(theme => ({
     marginRight: 10,
   },
   logoTitle: {
-    fontSize: 20,
+    fontSize: 24,
     fontStyle: 'italic',
     fontWeight: 500,
     letterSpacing: -1,
   },
+  signIn: {
+    fontSize: 16,
+    color: theme.palette.text.primary,
+  },
 }))
 
+// TODO: !currentUser -> signin render
 const Header = () => {
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <Logo className={classes.logo} />
-      <Typography className={classes.logoTitle} color="primary">
-        Study Buddies
-      </Typography>
+      <Link href="/" underline="none">
+        <div className={classes.logoWrapper}>
+          <Logo className={classes.logo} />
+          <Typography className={classes.logoTitle} color="primary">
+            Study Buddies
+          </Typography>
+        </div>
+      </Link>
+      <Link href={`${process.env.REACT_APP_SERVER_BASE_URL}/login`} underline="none">
+        <Typography className={classes.signIn} variant="h1">
+          Sign In
+        </Typography>
+      </Link>
     </div>
   )
 }
